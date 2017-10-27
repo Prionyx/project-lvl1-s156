26ef6f6be196ad9bff9a9b4b2c6decc4c2235e3e
Andrey Draganchuk <spirifex@gmail.com>
2017-10-27 15:41:43 +0300

    step 9

---
+++ src/games/Prime.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace BrainGames\Prime;
+
+function isPrime($num)
+{
+    if ($num < 2) {
+        return 'no';
+    }
+    $i = 2;
+    while ($i <= $num / 2) {
+        if ($num % $i == 0) {
+            return 'no';
+        }
+        $i += 1;
+    }
+    return 'yes';
+}
+
+function brain_isPrime()
+{
+    return function ($gamesCount) {
+        $hello = "Answer 'yes' if number is prime otherwise answer 'no'.";
+        $quest = [];
+        $correct = [];
+        for ($i = 0; $i < $gamesCount; $i++) {
+            $num = rand(1, 1000);
+            $quest[] = "$num";
+            $correct[] = isPrime($num);
+        }
+        return ['hello' => $hello, 'quest' => $quest, 'correct' => $correct];
+    };
+}
